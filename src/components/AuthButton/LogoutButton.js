cffc2ea6ad13e4fc45bd593cebfb5519e4205e91
Ricardo Mestre <ricardomestre@ricardos-air.lan>
2022-05-12 11:17:28 +0100

    fix the redirect after logout

--- src/components/AuthButton/LogoutButton.js
+++ src/components/AuthButton/LogoutButton.js
@@ -1,7 +1,7 @@
 import AuthButton from "./AuthButton";
 import { useAuth0 } from "@auth0/auth0-react";
 const LogoutButton = (props) => {
-  const { logout } = useAuth0();
+  const { logout } = useAuth0({ redirectTo: window.location.origin });
 
   return (
     <AuthButton onClick={logout} id="qa_logoutbutton" {...props}>
